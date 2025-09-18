bbbbc5326eb8bd69d05aa6e934787b6eb8eb6d6a
GabrielRalph <gltralph@gmail.com>
2025-09-18 13:51:27 +1000

    allow . files

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Serve static files from 'public'
-app.use(express.static("public"));
-
+app.use(
+  express.static(path.join(__dirname, "public"), {
+    dotfiles: "allow"  // <-- this is the key
+  })
+);
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
