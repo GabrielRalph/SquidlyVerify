4feafdd1d88d1d25bc8a1e2851a34b5a4ead559e
GabrielRalph <gltralph@gmail.com>
2025-09-18 14:01:53 +1000

    try again

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080;
 
 // Serve static files from 'public'
 app.use(
-  express.static(path.join(__dirname, "public"), {
+  express.static("public", {
     dotfiles: "allow"  // <-- this is the key
   })
 );
