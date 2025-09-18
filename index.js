777947b6d63cb5652b6932e94d779e96098bd7a5
GabrielRalph <gltralph@gmail.com>
2025-09-18 15:01:21 +1000

    ggg

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 8080;
 // Serve static files from 'public'
 app.use(express.static("public"));
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server GGG listening on port ${PORT}`);
 });
