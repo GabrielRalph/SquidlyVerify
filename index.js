9eee88df1331b5d0372b0673f0dc25b5f32a7837
GabrielRalph <gltralph@gmail.com>
2025-09-18 13:44:13 +1000

    add server

---
+++ index.js
@@ -0,0 +1,11 @@
+// server.js
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Serve static files from 'public'
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
