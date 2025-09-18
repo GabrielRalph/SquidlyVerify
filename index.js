39f9aa1d13f4a017f79c054f37fa30764c95b60a
GabrielRalph <gltralph@gmail.com>
2025-09-18 14:20:44 +1000

    path

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// server.js
 const express = require("express");
 const app = express();
+const path = require("path");
 const PORT = process.env.PORT || 8080;
 
 // Serve static files from 'public'
 app.use(
-  express.static("public", {
+  express.static(path.join(__dirname, "public"), {
     dotfiles: "allow" 
   })
 );
