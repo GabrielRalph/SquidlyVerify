ba2a4c26bba89d6768a4005d65b2d0908d2eaa2e
GabrielRalph <gltralph@gmail.com>
2025-09-20 14:11:28 +1000

    allow . again

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Serve static files from 'public'
-app.use(express.static("public"));
+app.use(express.static("public", {
+    dotfiles: 'allow',
+}));
 app.listen(PORT, () => {
   console.log(`Server GGG listening on port ${PORT}`);
 });
